e196425b3e4700a2dcae5adcbe9759d5dadbe3ed
Muhammad Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2024-04-27 09:48:48 +0600

    Time: 15 ms (52.08%), Space: 20.4 MB (57.68%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int num = nums[nums.size() - 1];
         int n = nums.size();
-
-        if (num >= n) {
-            long long int ZTN = (n * (n + 1)) / 2;
-            int sum = 0;
-
-            for (int i = 0; i < nums.size(); i++) {
-                sum += nums[i];
-            }
-
-            int missingNumber = (ZTN - sum);
-
-            return missingNumber;
+        int sum = (n * (n + 1))/2;
+        for(int i = 0; i < nums.size();i++){
+            sum -=nums[i];
         }
-        else{
-            return n;
-        }
+        return sum;
     }
 };
