81808a4e8228807fdbffc020aef6842916327013
Muhammad Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2024-04-28 10:51:44 +0600

    Time: 239 ms (12.21%), Space: 84 MB (8.14%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map<int, int> res;
+        int n;
+        bool isTrue = 0;
+
+        // first -> value -> index
+        // previously avaible in map cheak, (i - map[value]) 
+        // update index
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (res.count(nums[i])) {
+                n = i - res[nums[i]];
+                if (n <= k)
+                    isTrue = 1;
+                res[nums[i]] = i;
+            } else {
+                res[nums[i]] = i;
+            }
+        }
+        return isTrue;
+    }
+};
