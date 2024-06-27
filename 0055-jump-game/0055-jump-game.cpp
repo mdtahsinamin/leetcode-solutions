730ce3779fe6b52fc20357e903f0d6ff59754911
Muhammad Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2024-06-27 21:34:07 +0600

    Time: 48 ms (42.69%), Space: 50.8 MB (81.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        int lastIndex = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (maxReach < i) {
+                return false;
+            }
+            maxReach = max(maxReach, i + nums[i]);
+        }
+
+        
+            return true;
+        
+    }
+};
