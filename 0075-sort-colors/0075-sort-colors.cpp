b84a8a56f913fee0f24e92d3d2748924625cfc37
Muhammad Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2024-07-02 23:52:28 +0600

    Time: 5 ms (15.96%), Space: 9.8 MB (36.2%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+// Dutch National Flag algorithm
+
+/*
+ It takes three pointer
+ - low 
+ - mid
+ - high
+
+*/
