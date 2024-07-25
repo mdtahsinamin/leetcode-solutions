81b352afd5f96ed1ca6938cab67cb1c94c15b309
Muhammad Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2024-07-25 23:49:53 +0600

    Time: 56 ms (74.08%), Space: 52 MB (24.78%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+    return {
+        increment(){
+            return current += 1;
+        },
+        decrement(){
+            return current -=1;
+        },
+        reset(){
+            current = init;
+            return init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
