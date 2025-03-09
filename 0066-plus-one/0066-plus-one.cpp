05a80f5401b067da334e458ac4f33653dcd19f2a
Muhammad Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2025-03-09 15:07:53 +0600

    Time: 0 ms (100%), Space: 11 MB (99.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int carray = 0;
-        vector<int> res;
 
-        if (digits[0] == 9 && digits.size() == 1) {
-            return {1, 0};
+       vector<int>ans;
+
+        digits[digits.size()-1]+=1;
+        int carry=0;
+       for(int i=digits.size()-1;i>=0;i--){
+        if((digits[i] + carry) > 9){
+            ans.push_back(0);
+            carry=1;
         }
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] == 9 && carray == 1 && i!=0) {
-                res.push_back((digits[i] + 1) % 10);
-                carray = 1;
-            } else if (digits[i] == 9 && i == digits.size() - 1) {
-                res.push_back((digits[i] + 1) % 10);
-                carray = 1;
-            } else if (digits[i] <= 8 && i == digits.size() - 1) {
-                res.push_back(digits[i] + 1);
-                carray = 0;
-            } else if (carray == 1 && i == 0) {
-                if(digits[i]<=8)
-                 res.push_back(digits[i] + 1);
-                if(digits[i] == 9){
-                    res.push_back((digits[i] + 1)%10);
-                    res.push_back(1);
-                }
-                
-            } else if(digits[i] <= 8 && carray == 1){
-                res.push_back(digits[i] + 1);
-                carray = 0;
-            }
-            
-             else {
-                res.push_back(digits[i]);
-                carray = 0;
-            }
-        }
-        reverse(res.begin(), res.end());
-        return res;
+       else{
+        ans.push_back(digits[i] + carry);
+        carry=0;
+       }
+
+       }
+
+       if(carry==1)ans.push_back(1);
+       reverse(ans.begin(),ans.end());
+
+       return ans; 
     }
 };
-
-// [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
